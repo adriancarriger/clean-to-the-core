b74728593cf4d61a9652e24b841f07ae517e9a54
Adrian Carriger <mail@adrianandnicole.com>
2016-10-19 08:16:59 -0700

    feat: Prevent reimport of the CoreModule
    Angular [recommends](https://angular.io/docs/ts/latest/guide/ngmodule.html#!#core-for-root) to only import the CoreModule once and fail fast if it is imported incorrectly.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -15,4 +15,12 @@ import { WatchHeightDirective } from './watch-height.directive';
   exports: [LayoutComponent],
   declarations: [NavComponent, FooterComponent, LayoutComponent, WatchHeightDirective]
 })
-export class CoreModule { }
+export class CoreModule {
+
+  constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
