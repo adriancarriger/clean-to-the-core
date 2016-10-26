9f3563e13c173eab068d8d63506771f4b4fa4098
Adrian Carriger <mail@adrianandnicole.com>
2016-10-26 00:27:37 -0700

    docs(CoreModule): improve documentation
    - group `module-import-guard.ts` into the `CoreModule`
    - document `GlobalEventsService`

--- src/app/core/global-events.service.ts
+++ src/app/core/global-events.service.ts
@@ -1,16 +1,24 @@
 /**
  * @module CoreModule
  */ /** */
-
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-
+/**
+ * {@link GlobalEventsService} is reponsible for handling and dispatching global events such as
+ * `window` resizing, `body` scrolling, etc.  
+ */
 @Injectable()
 export class GlobalEventsService {
-
+  /**
+   * Creates {@link GlobalEventsService}
+   * @param window `@Inject` is used to allow a mock window during testing
+   * (following this [Stackover answer](http://stackoverflow.com/a/38875374/5357459)).
+   */
   constructor(@Inject('Window') window: Window) { }
-
-  resize() {
+  /**
+   * Allows a subscriber to listen to window resize events.
+   */
+  resize(): Observable<Event> {
     return Observable.fromEvent(window, 'resize');
   }
 
--- src/app/core/module-import-guard.ts
+++ src/app/core/module-import-guard.ts
@@ -1,3 +1,10 @@
+/**
+ * @module CoreModule
+ */ /** */
+/**
+ * Prevent Reimport of Core Module
+ * @see [STYLE 04-11](https://angular.io/styleguide#04-12)
+ */
 export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
   if (parentModule) {
     throw new Error(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
