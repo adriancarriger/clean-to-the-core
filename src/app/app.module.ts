a62ded55389cc67b8ee81a74708c61a430b47c0b
Adrian Carriger <mail@adrianandnicole.com>
2016-10-26 17:46:42 -0700

    style(AppModule): clean up unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@
  * @preferred
  */ /** */
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 /**
@@ -22,10 +20,7 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserModule,
     CoreModule.forRoot(),
-    FormsModule,
-    HttpModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
