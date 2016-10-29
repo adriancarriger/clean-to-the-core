696484008fbbcfeef57df38aff1ad9709ab9921c
Adrian Carriger <mail@adrianandnicole.com>
2016-10-28 22:19:35 -0700

    fix(test): import FilterComponent

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,6 +4,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
+import { FilterComponent } from './filter/filter.component';
 import { SharedModule } from '../shared/shared.module';
 
 describe('HomeComponent', () => {
@@ -13,7 +14,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ SharedModule ],
-      declarations: [ HomeComponent ]
+      declarations: [ FilterComponent, HomeComponent ]
     })
     .compileComponents();
   }));
