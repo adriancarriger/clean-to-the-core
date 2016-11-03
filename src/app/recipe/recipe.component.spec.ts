11b0f82bdb374982a235ed4304c9933eed5a9223
Adrian Carriger <mail@adrianandnicole.com>
2016-11-02 21:16:41 -0700

    test(RecipeModule): add mock services

--- src/app/recipe/recipe.component.spec.ts
+++ src/app/recipe/recipe.component.spec.ts
@@ -1,17 +1,55 @@
 /* tslint:disable:no-unused-variable */
-import { DebugElement } from '@angular/core';
+import { DebugElement, Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, Observable } from 'rxjs/Rx';
 
 import { RecipeComponent } from './recipe.component';
+import { ApiService } from '../core/api/api.service';
+
+@Injectable()
+class MockActivatedRoute {
+  snapshot = {
+    params: {
+      slug: 'test-slug-1'
+    }
+  };
+}
+
+@Injectable()
+export class MockApiService {
+  recipeObj;
+  events$;
+  data = {
+    id: 0,
+    blurb: 'blurb text'
+  };
+  constructor() {
+    this.events$ = new Subject();
+    this.recipeObj = this.events$.asObservable();
+  }
+  recipe(input) {
+    this.update();
+    return this.events$.asObservable();
+  }
+  update() {
+    this.events$.next(this.data);
+  }
+}
 
 describe('RecipeComponent', () => {
   let component: RecipeComponent;
   let fixture: ComponentFixture<RecipeComponent>;
-
+  let mockActivatedRoute = new MockActivatedRoute();
+  let mockApiService = new MockApiService();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipeComponent ]
+      declarations: [ RecipeComponent ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: ApiService, useValue: mockApiService }
+      ]
     })
     .compileComponents();
   }));
