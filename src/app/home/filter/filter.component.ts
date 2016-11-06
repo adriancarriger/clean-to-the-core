27df5ecbda79c02dec68aed50a4046790aa7dead
Adrian Carriger <mail@adrianandnicole.com>
2016-11-05 18:51:06 -0700

    docs(filter): adds general documentation

--- src/app/home/filter/filter.component.ts
+++ src/app/home/filter/filter.component.ts
@@ -14,9 +14,29 @@ import { GlobalEventsService } from '../../core/global-events/global-events.serv
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-  drawerOpen = false;
+  /**
+   * Ouputs events related to the drawer opening/closing.
+   */
   @Output() drawerEvent = new EventEmitter();
+  /**
+   * Current state of the drawer.
+   * 
+   * - The drawer is div that shows/hides extra filter options
+   * - Set to closed by default
+   */
+  drawerOpen = false;
+  /**
+   * Creates the {@link FilterComponent}
+   * @param globalEventsService used to subscribe to global events like scroll
+   */
   constructor(private globalEventsService: GlobalEventsService) { }
+  /**
+   * Subscribes to the global scroll event.
+   * 
+   * **On scroll:**
+   * - closes the drawer
+   * - sends a drawer event via the {@link drawerEvent} output.
+   */
   ngOnInit() {
     this.globalEventsService.scroll().subscribe( () => {
       this.drawerOpen = false;
